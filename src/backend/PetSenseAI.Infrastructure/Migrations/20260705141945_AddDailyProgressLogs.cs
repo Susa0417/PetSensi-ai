6a901c3ef53c9433f5ced9135cab9d3c114a503c
Susa0417 <suresh@smartiplace.net>
2026-07-05 09:22:33 -0500

    migration updated

---
+++ src/backend/PetSenseAI.Infrastructure/Migrations/20260705141945_AddDailyProgressLogs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetSenseAI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDailyProgressLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
